6984bcea77a79a8d4e8026ef6416ead88f64faf7
memo <memo33@users.noreply.github.com>
2023-10-08 15:31:25 +0200

    cleanup channel page

--- web/src/main/scala/sc4pac/web/ChannelPage.scala
+++ web/src/main/scala/sc4pac/web/ChannelPage.scala
@@ -2,8 +2,6 @@ package io.github.memo33.sc4pac
 package web
 
 import scala.scalajs.js
-// import scala.scalajs.js.annotation.JSExportTopLevel
-import scala.scalajs.js.annotation.JSGlobal
 import org.scalajs.dom
 import org.scalajs.dom.document
 
@@ -16,11 +14,7 @@ import sttp.client4.{basicRequest, Request, UriContext, Response, ResponseExcept
 import sttp.client4.upicklejson.asJson
 
 import scalatags.JsDom.all as H  // html tags
-import scalatags.JsDom.all.stringFrag
-import scalatags.JsDom.all.stringAttr
-import scalatags.JsDom.all.SeqFrag
-import scalatags.JsDom.all.intPixelStyle
-import scalatags.JsDom.all.stringStyle
+import scalatags.JsDom.all.{stringFrag, stringAttr, SeqFrag, intPixelStyle, stringStyle}
 
 object JsonData extends SharedData {
   opaque type Instant = String
@@ -39,17 +33,6 @@ object ChannelPage {
 
   lazy val backend = sttp.client4.fetch.FetchBackend()
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
-  }
-
-  // @JSExportTopLevel("addClickedMessage")
-  def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
-  }
-
   def main(args: Array[String]): Unit = {
     document.addEventListener("DOMContentLoaded", (e: dom.Event) => setupUI())
   }
@@ -58,7 +41,7 @@ object ChannelPage {
   def parseModule(pkgName: String): Either[String, BareModule] =
     pkgName match {
       case regexModule(group, name) => Right(BareModule(group = Organization(group), name = ModuleName(name)))
-      case _ => Left("malformed package name: <group>:<name>")
+      case _ => Left("Malformed package name: <group>:<name>")
     }
 
   def fetchPackage(module: BareModule): Future[Option[JsonData.Package]] = {
@@ -66,8 +49,7 @@ object ChannelPage {
     for {
       response <- basicRequest.get(url).response(asJson[JsonData.Package]).send(backend)
     } yield {
-      if (!response.is200)
-        None
+      if (!response.is200) None
       else response.body.toOption
     }
   }
@@ -90,7 +72,6 @@ object ChannelPage {
     val b = Seq.newBuilder[H.Frag]
     def add(label: String, child: H.Frag): Unit =
       b += H.tr(H.th(label), H.td(child))
-
     def mkPar(text: String) = text.trim.linesIterator.map(H.p(_)).toSeq
 
     // add("Name", pkg.name)
@@ -122,7 +103,6 @@ object ChannelPage {
           }
         )
     )
-    // TODO add variant descriptions
 
     // TODO add info about which dependencies belong to which variant
     val deps = pkg.variants
@@ -146,77 +126,22 @@ object ChannelPage {
   }
 
   def setupUI(): Unit = {
-
-    // val response: Future[Response[Either[ResponseException[String, Exception], JsonData.Package]]] =
-    //   basicRequest
-    //     .get(uri"http://localhost:8090/metadata/memo/demo-package/1.0/demo-package-1.0.json")
-    //     .response(asJson[JsonData.Package])
-    //     .send(backend)
-
-    // response.foreach(resp => {
-    //   println(resp.code)
-    //   println(resp.body)
-    // })
-
-    // val button = document.createElement("button")
-    // button.textContent = "Click me!"
-    // button.addEventListener("click", (e: dom.MouseEvent) => addClickedMessage())
-    // document.body.appendChild(button)
-
-    // appendPar(document.body, "Hello World")
-
     val urlParams = new dom.URLSearchParams(dom.window.location.search)
     val pkgName = urlParams.get("pkg")
     if (pkgName == null) {
-      appendPar(document.body, s"Pass query ?pkg=<group>:<name>")
+      document.body.appendChild(H.p(s"Pass query ", H.code("?pkg=<group>:<name>")).render)
     } else parseModule(pkgName) match {
-      case Left(err) => appendPar(document.body, s"Package: $err")
+      case Left(err) =>
+        document.body.appendChild(H.p(err).render)
       case Right(module) =>
         val output = H.p("Loading package ", pkgNameFrag(module, link = false), "…").render
-        // document.body.appendChild(H.p("Loading package ", pkgNameFrag(module, link = false), "…").render)
         document.body.appendChild(output)
         fetchPackage(module) foreach {
           case None =>
-            // appendPar(document.body, "Package: not found")
-            output.replaceWith(H.p("Package not found").render)
+            document.body.appendChild(H.p("Package not found").render)
           case Some(pkg) =>
-            // document.body.appendChild(pkgInfoFrag(pkg).render)
             output.replaceWith(pkgInfoFrag(pkg).render)
         }
     }
   }
 }
-
-/*
-import org.scalajs.dom
-import org.scalajs.dom.document
-import zio.*
-import zio.Clock.*
-
-object Hello extends zio.ZIOAppDefault {
-
-  def run = {
-    for {
-      _      <- Console.printLine("Starting progress bar demo.")
-      target <- ZIO.succeed(document.createElement("pre"))
-      _      <- ZIO.succeed(document.body.appendChild(target))
-      _      <- update(target).repeat(Schedule.spaced(1.seconds))
-    } yield ExitCode.success
-  }
-
-  def update(target: dom.Element) = {
-    for {
-      time   <- currentTime(java.util.concurrent.TimeUnit.SECONDS)
-      output <- ZIO.succeed(progress((time % 11).toInt, 10))
-      _      <- ZIO.succeed(target.innerHTML = output)
-    } yield ()
-  }
-
-  def progress(tick: Int, size: Int) = {
-    val bar_length = tick
-    val empty_length = size - tick
-    val bar = "#" * bar_length + " " * empty_length
-    s"$bar $bar_length%"
-  }
-}
-*/
