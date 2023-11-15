cc7393117cebdc97168604258a5825680311c737
memo <memo33@users.noreply.github.com>
2023-11-15 08:09:43 +0100

    adjust json serialization using custom tags and stringKeyRW

--- src/main/scala/sc4pac/api/message.scala
+++ src/main/scala/sc4pac/api/message.scala
@@ -14,6 +14,7 @@ sealed trait PromptMessage extends Message derives UP.ReadWriter {
 }
 object PromptMessage {
 
+  @upickle.implicits.key("/prompt/choice/update/variant")
   case class ChooseVariant(
     `package`: BareModule,
     label: String,
@@ -22,6 +23,7 @@ object PromptMessage {
     token: String = scala.util.Random.nextInt().toHexString
   ) extends PromptMessage derives UP.ReadWriter
 
+  @upickle.implicits.key("/prompt/confirmation/update/plan")
   case class ConfirmUpdatePlan(
     toRemove: Seq[ConfirmUpdatePlan.Pkg],
     toInstall: Seq[ConfirmUpdatePlan.Pkg],
@@ -29,9 +31,11 @@ object PromptMessage {
     token: String = scala.util.Random.nextInt().toHexString
   ) extends PromptMessage derives UP.ReadWriter
   object ConfirmUpdatePlan {
-    case class Pkg(`package`: BareModule, version: String, variant: Variant) derives UP.ReadWriter
+    case class Pkg(`package`: BareModule, version: String, variant: Variant)
+    given pkgRw: UP.ReadWriter[Pkg] = UP.stringKeyRW(UP.macroRW)  // serializes Variant as dictionary instead of array
   }
 
+  @upickle.implicits.key("/prompt/confirmation/update/warnings")
   case class ConfirmInstallation(
     warnings: Map[BareModule, Seq[String]],
     choices: Seq[String], // = Seq("yes", "no"),
@@ -39,6 +43,7 @@ object PromptMessage {
   ) extends PromptMessage derives UP.ReadWriter
 }
 
+@upickle.implicits.key("/prompt/response")
 case class ResponseMessage(token: String, body: String) extends Message derives UP.ReadWriter
 
 sealed trait ErrorMessage extends Message derives UP.ReadWriter {
@@ -46,8 +51,11 @@ sealed trait ErrorMessage extends Message derives UP.ReadWriter {
   def detail: String
 }
 object ErrorMessage {
+  @upickle.implicits.key("/error/generic")
   case class Generic(message: String, detail: String) extends ErrorMessage derives UP.ReadWriter
+  @upickle.implicits.key("/error/scope-not-initialized")
   case class ScopeNotInitialized(message: String, detail: String) extends ErrorMessage derives UP.ReadWriter
 }
 
+@upickle.implicits.key("/result")
 case class ResultMessage(result: String) extends Message
