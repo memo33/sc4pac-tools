d527a5e34d8e6303d455e94246a851b6a53ab789
memo <memo33@users.noreply.github.com>
2024-11-10 11:23:44 +0100

    automatically download cicdec in dist.sh script

--- src/scripts/dist.sh
+++ src/scripts/dist.sh
@@ -8,5 +8,10 @@ OUT="target/dist/sc4pac-${VERSION}.zip"
 rm -f "$OUT"
 zip --junk-paths "$OUT" target/scala-3.4.2/sc4pac-cli.jar src/scripts/sc4pac src/scripts/sc4pac.bat README.md
 
-# cicdec was manually installed at ./cicdec/ and is bundled with our software
+# download and install cicdec at ./cicdec/ if not present to bundle it with our software
+if [ ! -e "cicdec" ]
+then
+    curl -L https://github.com/Bioruebe/cicdec/releases/download/3.0.1/cicdec.zip > target/cicdec.zip
+    unzip -d cicdec target/cicdec.zip
+fi
 zip -r "$OUT" cicdec
