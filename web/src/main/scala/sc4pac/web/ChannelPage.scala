memo33/sc4pac-tools

commit e4b28b1c9a4c8c0c4434d415c1412fc1cd0e99e0

upgrade to new scala language semantics (deprecated implicits)
also fixes unused imports

web/src/main/scala/sc4pac/web/ChannelPage.scala
@@ -16,21 +16,26 @@ import sttp.client4.upicklejson.asJson
 import scalatags.JsDom.all as H  // html tags
 import scalatags.JsDom.all.{stringFrag, stringAttr, SeqFrag, intPixelStyle, stringStyle, bindNode}
 
-object JsonData extends SharedData {
+trait StringRwHelper {
+  // helps avoid given-resolution conflicts (after switch from implicits to givens)
+  def rawStringRwHelper: UP.ReadWriter[String] = UP.readwriter[String]
+}
+
+object JsonData extends SharedData with StringRwHelper {
   opaque type Instant = String
-  val instantRw = UP.readwriter[String]
+  override given instantRw: UP.ReadWriter[Instant] = rawStringRwHelper
   opaque type SubPath = String
-  val subPathRw = UP.readwriter[String]
+  override given subPathRw: UP.ReadWriter[SubPath] = rawStringRwHelper
   protected def categoryFromSubPath(subpath: SubPath) =
     if (subpath.contains("/")) Some(subpath.substring(0, subpath.indexOf("/")))
     else Option.unless(subpath.isEmpty)(subpath)
   opaque type Checksum = Map[String, String]
-  val checksumRw = UP.readwriter[Map[String, String]]
+  override given checksumRw: UP.ReadWriter[Checksum] = ??? // UP.readwriter[Map[String, String]]
   protected def emptyChecksum = Map.empty
   opaque type Uri = String
-  val uriRw = UP.readwriter[String]
+  override given uriRw: UP.ReadWriter[Uri] = rawStringRwHelper
   opaque type IncludeWithChecksum = Map[String, String]
-  val includeWithChecksumRw = UP.readwriter[Map[String, String]]
+  override given includeWithChecksumRw: UP.ReadWriter[IncludeWithChecksum] = UP.readwriter[Map[String, String]]
 
   private val regexModule = """([^:\s]+):([^:\s]+)""".r
   def parseModule(pkgName: String): Either[String, BareModule] =
@@ -39,7 +44,7 @@ object JsonData extends SharedData {
       case _ => Left(s"Malformed package name: <group>:<name> ($pkgName)")
     }
 
-  val bareModuleRw: UP.ReadWriter[BareModule] = UP.readwriter[String].bimap[BareModule](_.orgName,
+  override given bareModuleRw: UP.ReadWriter[BareModule] = rawStringRwHelper.bimap[BareModule](_.orgName,
     parseModule(_).left.map(e => throw new IllegalArgumentException(e)).merge)
 }
 

shared/shared/src/main/scala/sc4pac/shared.scala
@@ -2,7 +2,7 @@ package io.github.memo33
 package sc4pac
 
 import java.util.regex.Pattern
-import upickle.default.{ReadWriter, readwriter, macroRW}
+import upickle.default.{ReadWriter, macroRW}
 
 sealed trait BareDep {
   def orgName: String
@@ -42,28 +42,28 @@ object JsonRepoUtil {
   val channelContentsFilename = "sc4pac-channel-contents.json"  // only for JSON repositories
 }
 
-abstract class SharedData {
+trait SharedData {
 
   type Variant = Map[String, String]
 
   type Instant
-  implicit val instantRw: ReadWriter[Instant]
+  given instantRw: ReadWriter[Instant] = compiletime.deferred
 
   type SubPath
-  implicit val subPathRw: ReadWriter[SubPath]
+  given subPathRw: ReadWriter[SubPath] = compiletime.deferred
   protected def categoryFromSubPath(subpath: SubPath): Option[String]
 
   type Checksum
-  implicit val checksumRw: ReadWriter[Checksum]
+  given checksumRw: ReadWriter[Checksum] = compiletime.deferred
   protected def emptyChecksum: Checksum
 
   type IncludeWithChecksum
-  implicit val includeWithChecksumRw: ReadWriter[IncludeWithChecksum]
+  given includeWithChecksumRw: ReadWriter[IncludeWithChecksum] = compiletime.deferred
 
-  implicit val bareModuleRw: ReadWriter[BareModule]
+  given bareModuleRw: ReadWriter[BareModule] = compiletime.deferred
 
   type Uri
-  implicit val uriRw: ReadWriter[Uri]
+  given uriRw: ReadWriter[Uri] = compiletime.deferred
 
   case class Dependency(group: String, name: String, version: String) derives ReadWriter
 
@@ -102,7 +102,7 @@ abstract class SharedData {
     def version: String
   } /*derives ReadWriter*/  // <-- causes infinite compile loop, so manually define ReadWriter instead
   object PackageAsset {
-    implicit val packageAssetRw: ReadWriter[PackageAsset] = ReadWriter.merge(Asset.assetRw, Package.packageRw)
+    given packageAssetRw: ReadWriter[PackageAsset] = ReadWriter.merge(Asset.assetRw, Package.packageRw)
   }
 
   case class ArchiveType(format: String, version: String) derives ReadWriter
@@ -133,7 +133,7 @@ abstract class SharedData {
     //   Option(lastModified).map(java.time.Instant.parse)  // throws java.time.format.DateTimeParseException
     // }
 
-    implicit val assetRw: ReadWriter[Asset] = macroRW
+    given assetRw: ReadWriter[Asset] = macroRW
   }
 
   @upickle.implicits.key("Package")
@@ -155,7 +155,7 @@ abstract class SharedData {
     def toBareDep: BareModule = BareModule(Organization(group), ModuleName(name))
   }
   object Package {
-    implicit val packageRw: ReadWriter[Package] = macroRW
+    given packageRw: ReadWriter[Package] = macroRW
 
     def buildVariantChoices(variants: Seq[VariantData]): Seq[VariantChoice] = {
       val entries: Seq[(String, String)] = variants.iterator.flatMap { vd =>
@@ -245,7 +245,7 @@ abstract class SharedData {
   object Channel {
 
     private val channelRwDefault: ReadWriter[Channel] = macroRW
-    implicit val channelRw: ReadWriter[Channel] =
+    given channelRw: ReadWriter[Channel] =
       channelRwDefault.bimap[Channel](identity, { c =>
         if (c.stats != null) c else createAddStats(c.scheme, c.info, packages = c.packages, assets = c.assets, c.externalPackages, c.externalAssets)
       })

src/main/scala/sc4pac/Data.scala
@@ -19,14 +19,14 @@ object JsonData extends SharedData {
   override type Uri = java.net.URI
 
   // We use OffsetDateTime.parse instead of Instant.parse for compatibility with Java 8 to 11
-  implicit val instantRw: ReadWriter[java.time.Instant] =
+  override given instantRw: ReadWriter[java.time.Instant] =
     readwriter[String].bimap[java.time.Instant](_.toString(), Option(_).map(s => java.time.OffsetDateTime.parse(s.trim()).toInstant()).orNull)
 
-  implicit val pathRw: ReadWriter[NioPath] = readwriter[String].bimap[NioPath](_.toString(), java.nio.file.Paths.get(_))
+  given pathRw: ReadWriter[NioPath] = readwriter[String].bimap[NioPath](_.toString(), java.nio.file.Paths.get(_))
 
-  implicit val subPathRw: ReadWriter[os.SubPath] = readwriter[String].bimap[os.SubPath](_.toString(), os.SubPath(_))
+  override given subPathRw: ReadWriter[os.SubPath] = readwriter[String].bimap[os.SubPath](_.toString(), os.SubPath(_))
 
-  implicit val uriRw: ReadWriter[java.net.URI] = readwriter[String].bimap[java.net.URI](_.toString(), new java.net.URI(_))
+  override given uriRw: ReadWriter[java.net.URI] = readwriter[String].bimap[java.net.URI](_.toString(), new java.net.URI(_))
 
   private[sc4pac] def bareModuleRead(s: String) =
     Sc4pac.parseModule(s) match {
@@ -35,8 +35,8 @@ object JsonData extends SharedData {
     }
   // Wrapping with `stringKeyRW` is important for Api/packages.info, so that
   // BareModule can be serialized as key of JSON dictionary (instead of serializing Maps as arrays of arrays).
-  implicit val bareModuleRw: ReadWriter[BareModule] = stringKeyRW(readwriter[String].bimap[BareModule](_.orgName, bareModuleRead))
-  implicit val bareDepRw: ReadWriter[BareDep] = stringKeyRW(readwriter[String].bimap[BareDep](_.orgName, { (s: String) =>
+  override given bareModuleRw: ReadWriter[BareModule] = stringKeyRW(readwriter[String].bimap[BareModule](_.orgName, bareModuleRead))
+  given bareDepRw: ReadWriter[BareDep] = stringKeyRW(readwriter[String].bimap[BareDep](_.orgName, { (s: String) =>
     val prefix = Constants.sc4pacAssetOrg.value + ":"
     if (s.startsWith(prefix)) BareAsset(assetId = C.ModuleName(s.substring(prefix.length))) else bareModuleRead(s)
   }))
@@ -179,9 +179,9 @@ object JsonData extends SharedData {
     assets: Seq[Asset],
     redownload: Seq[BareModule] = Seq.empty,  // since scheme 2+
   ) derives ReadWriter {
-    def dependenciesWithAssets: Set[Resolution.Dep] =
+    def dependenciesWithAssets: Set[Dep] =
       (installed.map(_.toDepModule) ++ assets.map(DepAsset.fromAsset(_))).toSet
-    def packagesToReinstall: Set[Resolution.DepModule] =
+    def packagesToReinstall: Set[DepModule] =
       installed.iterator.filter(_.reinstall).map(_.toDepModule).toSet
     def packagesToRedownload: Set[BareModule] = redownload.toSet
 
@@ -306,13 +306,13 @@ object JsonData extends SharedData {
 
   protected def categoryFromSubPath(subpath: SubPath): Option[String] = subpath.segments0.headOption
 
-  implicit val checksumRw: ReadWriter[Checksum] =
+  override given checksumRw: ReadWriter[Checksum] =
     readwriter[Map[String, String]].bimap[Checksum](
       (checksum: Checksum) => checksum.sha256.map("sha256" -> Checksum.bytesToString(_)).toMap,
       (m: Map[String, String]) => Checksum(sha256 = m.get("sha256").map(Checksum.stringToBytes))
     )
 
-  implicit val etagRw: ReadWriter[zio.http.Header.ETag] =
+  given etagRw: ReadWriter[zio.http.Header.ETag] =
     readwriter[String].bimap[zio.http.Header.ETag](
       _.renderedValue,
       s => zio.http.Header.ETag.parse(s).getOrElse(throw new IllegalArgumentException(s"Invalid etag: $s")),
@@ -387,7 +387,7 @@ object JsonData extends SharedData {
 
   case class IncludeWithChecksum(include: String, sha256: ArraySeq[Byte])
 
-  implicit val includeWithChecksumRw: ReadWriter[IncludeWithChecksum] =
+  override given includeWithChecksumRw: ReadWriter[IncludeWithChecksum] =
     readwriter[Map[String, String]].bimap[IncludeWithChecksum](
       (data: IncludeWithChecksum) => Map("include" -> data.include, "sha256" -> Checksum.bytesToString(data.sha256)),
       (m: Map[String, String]) => IncludeWithChecksum(include = m("include"), sha256 = Checksum.stringToBytes(m("sha256"))),

src/main/scala/sc4pac/Downloader.scala
@@ -12,8 +12,6 @@ import sc4pac.JsonData as JD
 import sc4pac.error.Artifact2Error
 import zio.http.Header.ETag
 
-import Downloader.PartialDownloadSpec
-
 /** This downloader implementation is based on Coursier's downloader (released under Apache License Version 2.0):
   * https://github.com/coursier/coursier/blob/8d93005b56dd84770c062aeae6d7a12c53948596/modules/cache/jvm/src/main/scala/coursier/cache/internal/Downloader.scala
   *

src/main/scala/sc4pac/FileCache.scala
@@ -245,7 +245,7 @@ class FileCache private (
 
 object FileCache {
   def apply(location: java.io.File, pool: java.util.concurrent.ExecutorService): FileCache = {
-    import sc4pac.CoursierZio.*  // implicit coursier-zio interop
+    import sc4pac.CoursierZio.given  // implicit coursier-zio interop
     val csCache = CC.FileCache[Task]()
       .withLocation(location)
       .withPool(pool)

src/main/scala/sc4pac/MetadataRepository.scala
@@ -2,7 +2,6 @@ package io.github.memo33
 package sc4pac
 
 import coursier.core.{Versions, Version}
-import coursier.cache as CC
 import upickle.default.Reader
 import zio.{ZIO, IO, UIO, RIO}
 

src/main/scala/sc4pac/Resolution.scala
@@ -7,7 +7,7 @@ import scala.collection.immutable.TreeSeqMap
 
 import sc4pac.JsonData as JD
 import sc4pac.error.{Sc4pacAssetNotFound, Artifact2Error}
-import Resolution.{Dep, DepAsset, Links}
+import Resolution.{Dep, Links}
 
 /** Wrapper around Coursier's resolution mechanism with more stringent types for
   * our purposes.

src/main/scala/sc4pac/api/auth.scala
@@ -2,7 +2,7 @@ package io.github.memo33
 package sc4pac
 package api
 
-import zio.{ZIO, Task, UIO, IO}
+import zio.{ZIO, UIO, IO}
 import zio.Ref
 import zio.http.*
 import zio.Config.Secret

commit 8bd7b041ecd28e7e5e5ec207f9a9a7bd16874b74

web: fix channel page json-deserialization
bug introduced by e4b28b1c9a4c8c0c4434d415c1412fc1cd0e99e0

web/src/main/scala/sc4pac/web/ChannelPage.scala
@@ -19,6 +19,7 @@ import scalatags.JsDom.all.{stringFrag, stringAttr, SeqFrag, intPixelStyle, stri
 trait StringRwHelper {
   // helps avoid given-resolution conflicts (after switch from implicits to givens)
   def rawStringRwHelper: UP.ReadWriter[String] = UP.readwriter[String]
+  def rawStringMapRwHelper: UP.ReadWriter[Map[String, String]] = UP.readwriter[Map[String, String]]
 }
 
 object JsonData extends SharedData with StringRwHelper {
@@ -30,7 +31,7 @@ object JsonData extends SharedData with StringRwHelper {
     if (subpath.contains("/")) Some(subpath.substring(0, subpath.indexOf("/")))
     else Option.unless(subpath.isEmpty)(subpath)
   opaque type Checksum = Map[String, String]
-  override given checksumRw: UP.ReadWriter[Checksum] = ??? // UP.readwriter[Map[String, String]]
+  override given checksumRw: UP.ReadWriter[Checksum] = rawStringMapRwHelper
   protected def emptyChecksum = Map.empty
   opaque type Uri = String
   override given uriRw: UP.ReadWriter[Uri] = rawStringRwHelper